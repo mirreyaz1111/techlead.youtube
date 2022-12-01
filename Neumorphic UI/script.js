4839681223309f1e17e36148e66da8c388e7f149
Reyaz Mir <mac@Reyazs-MacBook-Pro.local>
2022-12-01 22:02:07 +0530

    added part 3, tabs, search box, icon btns, slider

--- Neumorphic UI/script.js
+++ Neumorphic UI/script.js
@@ -10,6 +10,13 @@ const pause = document.querySelector('.pause');
 const playBtn = document.querySelector('.circle__btn');
 const wave1 = document.querySelector('.circle__back-1');
 const wave2 = document.querySelector('.circle__back-2');
+
+// Range slider
+const container = document.querySelector('.slider__box');
+const btn = document.querySelector('.slider__btn');
+const color = document.querySelector('.slider__color');
+const tooltip = document.querySelector('.slider__tooltip');
+
 const rotation = (target, val) => {
   target.style.transform = `rotate(${val}deg)`;
 }
@@ -41,3 +48,52 @@ playBtn.addEventListener('click', (e) => {
   wave2.classList.toggle('paused');
 })
 
+
+dragElement = (target, btn) => {
+  target.addEventListener('mousedown', (e) => {
+    onMouseMove(e);
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
+  });
+
+  onMouseMove = (e) => {
+    e.preventDefault();
+    let targetRect = target.getBoundingClientRect();
+    let x = e.pageX - targetRect.left + 10;
+    if (x > targetRect.width) { x = targetRect.width };
+    if (x < 0) { x = 0 };
+    btn.x = x - 10;
+    btn.style.left = btn.x + 'px';
+
+    // get the position of the button inside the container (%)
+    let percentPosition = (btn.x + 10) / targetRect.width * 100;
+
+    // color width = position of button (%)
+    color.style.width = percentPosition + "%";
+
+    // move the tooltip when button moves, and show the tooltip
+    tooltip.style.left = btn.x - 5 + 'px';
+    tooltip.style.opacity = 1;
+
+    // show the percentage in the tooltip
+    tooltip.textContent = Math.round(percentPosition) + '%';
+  };
+
+  onMouseUp = (e) => {
+    window.removeEventListener('mousemove', onMouseMove);
+    tooltip.style.opacity = 0;
+
+    btn.addEventListener('mouseover', function () {
+      tooltip.style.opacity = 1;
+    });
+
+    btn.addEventListener('mouseout', function () {
+      tooltip.style.opacity = 0;
+    });
+  };
+};
+
+dragElement(container, btn);
+
+// Thanks for watching
+// Please like and subscribe
