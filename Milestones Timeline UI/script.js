7acad488b28c74a69f100f8f8db5001797062e88
Mac <mac@Macs-MacBook-Pro.local>
2023-01-02 12:54:02 +0530

    added Milestone timeline

---
+++ Milestones Timeline UI/script.js
@@ -0,0 +1,26 @@
+let items = document.querySelectorAll(".timeline li");
+
+const isElementInViewport = (el) => {
+    let rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+const callbackFunc = () => {
+    for (let i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+            if (!items[i].classList.contains("in-view")) {
+                items[i].classList.add("in-view");
+            }
+        } else if (items[i].classList.contains("in-view")) {
+            items[i].classList.remove("in-view");
+        }
+    }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
